211542fa53456930320b3b525762683fba202267
nonthawat wattanawiwatwach <nonthawat2554963@gmail.com>
2024-02-22 16:03:33 +0700

    Add project files.

---
+++ ServiceBMISizq.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="ServiceBMISizq.asmx.cs" Class="ServiceBMISize.ServiceBMISizq" %>
---
+++ ServiceBMISizq.asmx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace ServiceBMISize
+{
+    /// <summary>
+    /// Summary description for ServiceBMISizq
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class ServiceBMISizq : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string GetSize(double weight, double height, double age)
+        {
+            string size = "Unknown";
+
+            if (height >= 140 && height <= 200)
+            {
+                if (weight >= 48.38 && weight <= 120.03)
+                {
+                    if (age >= 15 && age <= 60)
+                    {
+                        double bmi = weight / ((height / 100) * (height / 100));
+
+                        if (bmi < 18.5)
+                        {
+                            size = "YOUR SIZE IS : S";
+                        }
+                        else if (bmi < 24.9)
+                        {
+                            size = "YOUR SIZE IS : M";
+                        }
+                        else if (bmi < 29.9)
+                        {
+                            size = "YOUR SIZE IS : L";
+                        }
+                        else
+                        {
+                            size = "YOUR SIZE IS : XL";
+                        }
+                    }
+                }
+            }
+
+            return size;
+        }
+    }
+}
